d8f2895e1f1ba614dcd91be2f2621d0db4f0d00c
Marcus Ramirez <macusramirez@gmail.com>
2022-02-14 15:11:43 -0600

    data binding and routing working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CdsComponent } from './cds/cds.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [{path: "cds", component: CdsComponent}];
+const routes: Routes = [
+  {path: "cds", component: CdsComponent},
+  {path: "", component: HomePageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
